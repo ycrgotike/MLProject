fdc4f53fdb02a4641705f9f41af0c760587efb14
Yashwanth C Reddy <gycrgotike@gmail.com>
2026-02-23 14:48:08 +0100

    data transformation added

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = "{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
+# Create logs directory
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Create dynamic log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format = "[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO
-
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
-
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+# This has common functionality which the entire project can use
+
+import sys #Python runtime - CLI scripts, debugging imports, exiting
+import os #interface between my script and operating system - Files, folders, environment variables
+import dill
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
